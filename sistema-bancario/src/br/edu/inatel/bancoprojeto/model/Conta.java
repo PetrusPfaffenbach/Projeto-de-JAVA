4ee9bece47d9dd9b27549ff989a0a31d531cc114
Petrus <petrussouza1@gmail.com>
2026-06-03 16:12:47 -0300

    feat: adição de funcionamentos da class contas

--- sistema-bancario/src/br/edu/inatel/bancoprojeto/model/Conta.java
+++ sistema-bancario/src/br/edu/inatel/bancoprojeto/model/Conta.java
@@ -1,5 +1,34 @@
 package br.edu.inatel.bancoprojeto.model;
 
 public abstract class Conta {
+    private int numero;
+    private double saldo;
+
+    public void sacar(double quantia) {
+        if(this.saldo >= quantia) {
+            this.saldo -= quantia;
+            System.out.println("Saque realizado com suceso");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public void depositar(double quantia) {
+        if(quantia > 0) {
+            this.saldo += quantia;
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 
 }
